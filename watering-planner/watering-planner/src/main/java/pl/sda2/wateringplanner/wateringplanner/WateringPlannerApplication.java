f6e6924ba39ceeb970d8150ace73b5471d134fbe
darkhir <98260400+darkhir@users.noreply.github.com>
2022-07-17 10:00:12 +0200

    Add files via upload

---
+++ watering-planner/watering-planner/src/main/java/pl/sda2/wateringplanner/wateringplanner/WateringPlannerApplication.java
@@ -0,0 +1,13 @@
+package pl.sda2.wateringplanner.wateringplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WateringPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WateringPlannerApplication.class, args);
+	}
+
+}
---
+++ watering-planner/watering-planner/src/test/java/pl/sda2/wateringplanner/wateringplanner/WateringPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda2.wateringplanner.wateringplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WateringPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
