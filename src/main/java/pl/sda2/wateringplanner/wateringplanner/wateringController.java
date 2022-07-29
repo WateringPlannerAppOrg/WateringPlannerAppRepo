f9c082ff3febd9c69013ae1913ea6adb03077993
elSenorr <seweryn.kreft@gmail.com>
2022-07-29 20:48:48 +0200

    - frontend add

--- src/main/java/pl/sda2/wateringplanner/wateringplanner/wateringController.java
+++ src/main/java/pl/sda2/wateringplanner/wateringplanner/wateringController.java
@@ -3,15 +3,19 @@ package pl.sda2.wateringplanner.wateringplanner;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @Slf4j
-@Controller
+@RestController
 public class wateringController {
 
 
     @GetMapping("/")
+
     public String index() {
-        return "index";
+
+
+        return "plant-list";
     }
 }
